538cd7b836029414bac1ab567e83c281fedac37a
nubik2409-star <nubik2409@gmail.com>
2026-03-01 21:05:05 +0100

    Add debug information for client IP addresses

--- public/debug.php
+++ public/debug.php
@@ -1,6 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
 <?php
 echo 'REMOTE_ADDR: ' . $_SERVER['REMOTE_ADDR'] . '<br>';
 echo 'CF_CONNECTING_IP: ' . ($_SERVER['HTTP_CF_CONNECTING_IP'] ?? 'not set') . '<br>';
 echo 'X_FORWARDED_FOR: ' . ($_SERVER['HTTP_X_FORWARDED_FOR'] ?? 'not set') . '<br>';
 echo 'X_REAL_IP: ' . ($_SERVER['HTTP_X_REAL_IP'] ?? 'not set') . '<br>';
 ?>
+
+</body>
+</html>
